f01a75672bd444e8a509dd0bc853544fe5d07c43
Pedro Henrique G Moreira <pedro.moreira@coopersystem.com.br>
2020-01-28 18:23:34 -0300

    feat: bus

---
+++ src/Modelo.Domain.Core/Bus/IMediatorHandler.cs
@@ -0,0 +1,18 @@
+﻿using Modelo.Domain.Core.Commands;
+using Modelo.Domain.Core.Events;
+using MediatR;
+using System.Threading.Tasks;
+
+namespace Modelo.Domain.Core.Bus
+{
+    public interface IMediatorHandler
+    {
+        Task PublishCommand<T>(T command) where T : Command;
+
+        Task PublishMessage<T>(T message) where T : Message;
+
+        Task<TResponse> SendCommand<TResponse>(IRequest<TResponse> command);
+
+        Task RaiseEvent<T>(T @event) where T : Event;
+    }
+}
