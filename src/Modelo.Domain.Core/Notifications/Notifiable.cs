028dcd464d2db53d4030a4577d3cf682426bc22a
Pedro Henrique G Moreira <pedro.moreira@coopersystem.com.br>
2020-01-28 18:23:46 -0300

    feat: notificações

---
+++ src/Modelo.Domain.Core/Notifications/Notifiable.cs
@@ -0,0 +1,54 @@
+﻿using Modelo.Domain.Core.Bus;
+using Modelo.Domain.Core.Commands;
+using FluentValidation.Results;
+using System.Threading.Tasks;
+using MediatR;
+
+namespace Modelo.Domain.Core.Notifications
+{
+    public abstract class Notifiable
+    {
+        private readonly IMediatorHandler _bus;
+        private readonly NotificationHandler _notifications;
+
+        protected Notifiable(IMediatorHandler bus, INotificationHandler<Notification> notifications)
+        {
+            _bus = bus;
+            _notifications = (NotificationHandler)notifications;
+        }
+
+        protected virtual async Task ValidateAndNotifyValidationErrors(Command message)
+        {
+            await message.IsValid();
+            await NotifyValidationErrors(message);
+        }
+
+        protected virtual Task NotifyValidationErrors(Command message)
+        {
+            return NotifyValidationErrors(message.ValidationResult);
+        }
+
+        protected virtual async Task NotifyValidationErrors(ValidationResult validationResult)
+        {
+            foreach (var error in validationResult.Errors)
+            {
+                await Notify(error.PropertyName, error.ErrorMessage);
+            }
+        }
+
+        protected virtual Task Notify(string key, string value)
+        {
+            return Notify(new Notification(key, value));
+        }
+
+        protected virtual Task Notify(Notification notification)
+        {
+            return _bus.RaiseEvent(notification);
+        }
+
+        protected virtual bool IsValid()
+        {
+            return !_notifications.HasNotifications();
+        }
+    }
+}
---
+++ src/Modelo.Domain.Core/Notifications/NotificationHandler.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+
+namespace Modelo.Domain.Core.Notifications
+{
+    public class NotificationHandler : INotificationHandler<Notification>
+    {
+        private List<Notification> _notifications;
+
+        public NotificationHandler()
+        {
+            _notifications = new List<Notification>();
+        }
+
+        public Task Handle(Notification message, CancellationToken cancellationToken)
+        {
+            _notifications.Add(message);
+
+            return Task.CompletedTask;
+        }
+
+        public virtual List<Notification> GetNotifications()
+        {
+            return _notifications;
+        }
+
+        public virtual bool HasNotifications()
+        {
+            return GetNotifications().Any();
+        }
+
+        public void Dispose()
+        {
+            _notifications = new List<Notification>();
+        }
+    }
+}
